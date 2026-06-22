c30ba6f8503f5bbed7842f932e1fc58c9552b8fe
Ting Hong Zhang <HTING007@e.ntu.edu.sg>
2026-06-22 23:51:34 +0800

    Add user authentication system

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def login_user(request):
+
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+
+    return render(request,'login.html')
