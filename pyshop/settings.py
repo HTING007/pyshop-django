f946baac64cc7a182a609de6114ca040eb573e7b
Ting Hong Zhang <HTING007@e.ntu.edu.sg>
2026-06-28 23:52:10 +0800

    Add remove item and homepage add to cart

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
-    'accounts'
+    'accounts',
+    'cart'
 ]
 
 MIDDLEWARE = [
