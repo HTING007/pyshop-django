8e018ecded366cc2c2fa1f40837739e3eeaf4489
Ting Hong Zhang <HTING007@e.ntu.edu.sg>
2026-07-05 14:18:52 +0800

    Use POST for cart quantity updates

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.cart,name='cart'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart')
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('increase/<int:item_id>/',views.increase_quantity,name='increase_quantity'),
+    path('decrease/<int:item_id>/',views.decrease_quantity,name='decrease_quantity')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from products.models import Product
 from .models import CartItem
+from django.views.decorators.http import require_POST
 
 
 def cart(request):
     cart_items = CartItem.objects.filter(user=request.user)
+    cart_count = CartItem.objects.filter(user=request.user).count()
 
     total = 0
     for item in cart_items:
@@ -12,7 +14,8 @@ def cart(request):
 
     return render(request,'cart.html',{
         'cart_items': cart_items,
-        'total': total
+        'total': total,
+        'cart_count': cart_count
     })
 
 def add_to_cart(request,product_id):
@@ -32,4 +35,21 @@ def add_to_cart(request,product_id):
 def remove_from_cart(request,item_id):
     item = get_object_or_404(CartItem,id=item_id,user=request.user)
     item.delete()
+    return redirect('cart')
+
+@require_POST
+def decrease_quantity(request,item_id):
+    item = get_object_or_404(CartItem,id=item_id,user=request.user)
+    if item.quantity > 1:
+        item.quantity -= 1
+        item.save()
+    else:
+        item.delete()
+    return redirect('cart')
+
+@require_POST
+def increase_quantity(request,item_id):
+    item = get_object_or_404(CartItem,id=item_id,user=request.user)
+    item.quantity += 1
+    item.save()
     return redirect('cart')
