fca4489ebb5ee2999490d7f41cb446faa28fa9b2
Ting Hong Zhang <HTING007@e.ntu.edu.sg>
2026-07-11 09:49:52 +0800

    Handle anonymous users in cart context processor

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,13 +1,17 @@
 from .models import CartItem
 
 def cart_count(request):
+    if not request.user.is_authenticated:
+        return{
+            'cart_count': 0
+        }
+
     cart_items = CartItem.objects.filter(user=request.user)
 
-    cart_count = 0
-
+    total_quantity = 0
     for item in cart_items:
-        cart_count += item.quantity
+        total_quantity += item.quantity
 
     return {
-        'cart_count': cart_count
+        'cart_count': total_quantity
     }
