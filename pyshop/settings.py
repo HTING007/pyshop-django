2e82f349df1ebd244f90fddeb4818f08e78ae032
Ting Hong Zhang <HTING007@e.ntu.edu.sg>
2026-07-11 10:00:28 +0800

    Fix production secret key configuration

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    '50ddc340fbef5fe0c8c31b9207c33f9b',
+    'SECRET_KEY',
     'django-insecure-local-development-key'
 )
 
